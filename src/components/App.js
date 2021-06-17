9b63041c3c34b46b998669ff0143a0db3320ff18
Zil Norvilis <zilasino27@gmail.com>
2021-06-17 10:19:14 +0200

    Add Item module

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import Nav from "./Nav";
+import ItemPanel from "./ItemPanel";
+
+const App = () => {
+  return (
+    <div className="App">
+      <Nav />
+      <ItemPanel />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Item.js
@@ -0,0 +1,14 @@
+const Item = () => {
+  return (
+    <span>
+      <div>
+        <h3>Name</h3>
+      </div>
+      <div>
+        <img src="https://via.placeholder.com/150" />
+      </div>
+    </span>
+  );
+};
+
+export default Item;
