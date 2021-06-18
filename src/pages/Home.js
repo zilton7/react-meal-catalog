5efacdf1157550d79a89f75c5d24143f8984f1cf
Zil Norvilis <zilasino27@gmail.com>
2021-06-18 16:41:56 +0200

    Add Filter

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,5 @@
 import { Game } from "phaser";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { loadMeals } from "../actions/mealsAction";
 import Meal from "../components/Meal";
@@ -7,15 +7,33 @@ import MealDetail from "../components/MealDetail";
 import { useLocation } from "react-router-dom";
 
 const Home = () => {
+  const [input, setInput] = useState("");
+  const handleChange = (e) => {
+    e.preventDefault();
+    setInput(e.target.value);
+  };
   const location = useLocation();
   const pathId = location.pathname.split("/")[2];
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(loadMeals());
   }, [dispatch]);
-  const meals = useSelector((state) => state.meals.meals);
+  let meals = useSelector((state) => state.meals.meals);
+
+  if (input.length > 0) {
+    meals = meals.filter((meal) => {
+      return meal.strMeal.match(input);
+    });
+  }
+
   return (
     <div>
+      <input
+        type="text"
+        placeholder="Search for Meal"
+        onChange={handleChange}
+        value={input}
+      />
       {pathId && <MealDetail />}
       <h2>Meals</h2>
       <div className="meals">
